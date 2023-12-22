1d2c40f7334e9ac86ff34eea42e749661edfbdae
sakurasou3 <ko.ki.0216@gmail.com>
2023-12-22 09:32:05 +0900

    add CRUD page

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
-    path("", TaskList.as_view()),
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task/", TaskCreate.as_view(), name="create-task"),
+    path("edit-task/<int:pk>/", TaskUpdate.as_view(), name="edit-task"),
+    path("delete-task/<int:pk>/", TaskDelete.as_view(), name="delete-task"),
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from todoapp.models import Task
 
@@ -8,4 +9,30 @@ from todoapp.models import Task
 # Create your views here.
 class TaskList(ListView):
     model = Task
-    context_object_name = 'tasks'
+    context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    # fields = ['title', 'description', ...]と同じ
+    fields = "__all__"
+    # リダイレクト
+    success_url = reverse_lazy("tasks")
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = "task"
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
